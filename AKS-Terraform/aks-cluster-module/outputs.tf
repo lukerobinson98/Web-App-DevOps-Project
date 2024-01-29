997f72ce41d94842e3f85827a6c87546ca1d7609
lukerobinson98 <lukerobinson8@hotmail.co.uk>
2024-01-29 20:26:10 +0000

    Debugged Terraform configuration and resolved syntax errors

--- AKS-Terraform/aks-cluster-module/outputs.tf
+++ AKS-Terraform/aks-cluster-module/outputs.tf
@@ -1,15 +1,15 @@
 
 output "aks_cluster_name" {
     description = "The name of the provisioned AKS cluster"
-    value = azurerm_kubernetes_cluster.aks_cluster_name
+    value = azurerm_kubernetes_cluster.aks_cluster.name
 }
 
 output "aks_cluster_id" {
     description = "The ID of the provisioned AKS cluster"
-    value = azurerm_kubernetes_cluster.aks_cluster_id
+    value = azurerm_kubernetes_cluster.aks_cluster.id
 }
 
 output "aks_kubeconfig" {
     description = "The Kubernetes configuration file for the provisioned AKS cluster"
-    value = azurerm_kubernetes_cluster.aks_cluster.kube_config[0]["raw_config"]
+    value = azurerm_kubernetes_cluster.aks_cluster.kube_config[0]
 }
